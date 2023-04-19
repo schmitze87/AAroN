f4a3ae6af207153985a01041666918719eace3f3
Markus Schmitz <Schmitze87@gmail.com>
2023-04-20 00:27:05 +0200

    Start working on Neo4j 5.6.0

--- AAroN-Framework/src/main/java/aaron/util/Util.java
+++ AAroN-Framework/src/main/java/aaron/util/Util.java
@@ -2,22 +2,27 @@ package aaron.util;
 
 import org.neo4j.graphdb.Result;
 import org.neo4j.graphdb.Transaction;
-import org.neo4j.string.HexString;
 
 import java.io.*;
 import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.time.Instant;
+import java.util.HexFormat;
 import java.util.Map;
 
 public class Util {
 
+    private Util() {
+
+    }
+
     public static String getImportFolder(Transaction t) {
         String path = "/import";
-        Result result = t.execute("CALL dbms.listConfig() YIELD name, value \n"
-                + "WHERE name = 'dbms.directories.import' \n" +
-                "RETURN value;");
+        Result result = t.execute("""
+CALL dbms.listConfig() YIELD name, value
+WHERE name = 'server.directories.import'
+RETURN value;""");
         if (result.hasNext()) {
             Map<String, Object> next = result.next();
             Object value = next.get("value");
@@ -35,34 +40,30 @@ public class Util {
         dos.writeLong(timestamp.getEpochSecond());
         dos.flush();
         digest.update(bos.toByteArray());
-        return HexString.encodeHexString(digest.digest());
+        return HexFormat.of().formatHex(digest.digest());
     }
 
     public static String createSHA1(File file) throws Exception {
         MessageDigest digest = MessageDigest.getInstance("SHA-1");
-        InputStream fis = new FileInputStream(file);
-        int n = 0;
-        byte[] buffer = new byte[8192];
-        while (n != -1) {
-            n = fis.read(buffer);
-            if (n > 0) {
-                digest.update(buffer, 0, n);
-            }
-        }
-        return HexString.encodeHexString(digest.digest());
+        return getCRCString(file, digest);
     }
 
     public static String createSHA256(File file) throws Exception {
         MessageDigest digest = MessageDigest.getInstance("SHA-256");
-        InputStream fis = new FileInputStream(file);
-        int n = 0;
-        byte[] buffer = new byte[8192];
-        while (n != -1) {
-            n = fis.read(buffer);
-            if (n > 0) {
-                digest.update(buffer, 0, n);
+        return getCRCString(file, digest);
+    }
+
+    private static String getCRCString(File file, MessageDigest digest) throws IOException {
+        try (InputStream fis = new FileInputStream(file)) {
+            int n = 0;
+            byte[] buffer = new byte[8192];
+            while (n != -1) {
+                n = fis.read(buffer);
+                if (n > 0) {
+                    digest.update(buffer, 0, n);
+                }
             }
         }
-        return HexString.encodeHexString(digest.digest());
+        return HexFormat.of().formatHex(digest.digest());
     }
 }
