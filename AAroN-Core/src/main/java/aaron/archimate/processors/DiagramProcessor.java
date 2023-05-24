7acf5caf962ee07063bb5f800b4f5a003b50eb4b
Markus Schmitz <mschmitz@conet.de>
2023-05-24 20:18:12 +0200

    Work on Archimate Diagram handling

--- AAroN-Core/src/main/java/aaron/archimate/processors/DiagramProcessor.java
+++ AAroN-Core/src/main/java/aaron/archimate/processors/DiagramProcessor.java
@@ -1,8 +1,8 @@
 package aaron.archimate.processors;
 
-import aaron.archimate.exchangexml.Diagram;
-import aaron.archimate.exchangexml.LangStringType;
+import aaron.archimate.exchangexml.*;
 import aaron.archimate.identifier.ArchiMateIdentifier;
+import aaron.model.AAroNEdge;
 import aaron.model.AAroNNode;
 import aaron.model.Model;
 
@@ -30,6 +30,48 @@ public class DiagramProcessor extends AbstractProcessor<Diagram> {
                 .addProperty("name", STRING, name)
                 .addProperty("identifier", STRING, identifier)
                 .build();
-        model.addNode(new ArchiMateIdentifier(identifier), node);
+        ArchiMateIdentifier diagramIdentifier = new ArchiMateIdentifier(identifier);
+        model.addNode(diagramIdentifier, node);
+
+        for (ViewNodeType viewNode : diagram.getNode()) {
+            handleViewNode(diagramIdentifier, viewNode, null);
+        }
+    }
+
+    private void handleViewNode(ArchiMateIdentifier diagramIdentifier, ViewNodeType viewNode, ArchiMateIdentifier parentViewNode) {
+        if (viewNode instanceof Label label) {
+            //Skip this as it has not much value for the graph. At least for now
+            return;
+        }
+        if (viewNode instanceof Element element) {
+            Object elementRef = element.getElementRef();
+            ArchiMateIdentifier elementIdentifier = null;
+            if (elementRef instanceof DataObject dataObject) {
+                elementIdentifier = new ArchiMateIdentifier(dataObject.getIdentifier());
+                AAroNEdge edge = AAroNEdge.builder()
+                        .setStart(diagramIdentifier)
+                        .setEnd(elementIdentifier)
+                        .setType("SHOWS")
+                        .build();
+                model.addEdge(new ArchiMateIdentifier(viewNode.getIdentifier()), edge);
+            }
+            for (ViewNodeType childViewNode : element.getNode()) {
+                handleViewNode(diagramIdentifier, childViewNode, elementIdentifier);
+                if (elementIdentifier != null) {
+                    AAroNEdge edge = AAroNEdge.builder()
+                            .setStart(diagramIdentifier)
+                            .setEnd(elementIdentifier)
+                            .setType("EMBEDS")
+                            .build();
+                    model.addEdge(new ArchiMateIdentifier(viewNode.getIdentifier()), edge);
+                }
+            }
+            return;
+        }
+        if (viewNode instanceof Container container) {
+            for (ViewNodeType childViewNode : container.getNode()) {
+                handleViewNode(diagramIdentifier, childViewNode, null);
+            }
+        }
     }
 }
