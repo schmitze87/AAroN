ad5c529ea1babc85ff4617c08c7199c6ec18e481
Markus Schmitz <mschmitz@conet.de>
2023-09-21 00:31:03 +0200

    Increase version number and update dependencies

--- AAroN-Docker/build.sh
+++ AAroN-Docker/build.sh
@@ -4,4 +4,4 @@ directory=$(dirname "$(readlink -f "$0")")
 cd "$directory" || exit
 
 echo "Build AAroN Container Image"
-docker build -f ./Dockerfile --pull -t mschmitze87/aaron:neo4j-4.4.19_1 -t mschmitze87/aaron:latest .
+docker build -f ./Dockerfile --pull -t mschmitze87/aaron:neo4j-4.4.25_0 -t mschmitze87/aaron:latest .
