6f5b679dc5ce070eb328f818acccda023b894720
Markus Schmitz <mschmitz@conet.de>
2024-07-12 09:46:47 +0200

    update neo4j container version

--- AAroN-Docker/build.sh
+++ AAroN-Docker/build.sh
@@ -4,4 +4,4 @@ directory=$(dirname "$(readlink -f "$0")")
 cd "$directory" || exit
 
 echo "Build AAroN Container Image"
-docker build -f ./Dockerfile --pull -t mschmitze87/aaron:neo4j-4.4.34 -t mschmitze87/aaron:latest .
+docker build -f ./Dockerfile --pull -t mschmitze87/aaron:neo4j-4.4.35 -t mschmitze87/aaron:latest .
