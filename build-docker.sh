b2c19160186d4c5355abcebb137ab876715d2b48
Markus Schmitz <3823750+schmitze87@users.noreply.github.com>
2025-07-23 17:41:36 +0200

    Docker build script

--- build-docker.sh
+++ build-docker.sh
@@ -3,14 +3,13 @@
 directory=$(dirname "$(readlink -f "$0")")
 cd "$directory" || exit
 
-cd ./AAroN-Docker || exit
-
-echo "Build AAroN Container Image"
-
-VERSION=$(docker run -q --rm -v "$(pwd)/..":/usr/src/mymaven -w /usr/src/mymaven maven:3.9-eclipse-temurin-11-alpine mvn --non-recursive help:evaluate -Dexpression=project.version -q -DforceStdout)
+VERSION=$(docker run -q --rm -v "$(pwd)":/usr/src/mymaven -w /usr/src/mymaven maven:3.9-eclipse-temurin-11-alpine mvn help:evaluate -Dexpression=version.neo4j -q -DforceStdout)
 echo "code version $VERSION"
 NEO4J_VERSION=${VERSION//-SNAPHSOT/}
 echo "neo4j version $NEO4J_VERSION"
 NEO4J_TAG=$(echo -n "${NEO4J_VERSION}-community")
 echo "neo4j container tag $NEO4J_TAG"
+
+cd ./AAroN-Docker || exit
+echo "Build AAroN Container Image"
 docker build -f ./Dockerfile --pull --no-cache --build-arg NEO4J_VERSION=$NEO4J_TAG -t mschmitze87/aaron:latest -t mschmitze87/aaron:$NEO4J_VERSION .
