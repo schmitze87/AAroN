c32123af9483dcbc6eff38ef25c0540f65176780
Markus Schmitz <3823750+schmitze87@users.noreply.github.com>
2025-08-21 11:15:39 +0200

    Fallback jdbc connection string

--- AAroN-Core/src/main/java/aaron/sparx/SparxMSSQLConverter.java
+++ AAroN-Core/src/main/java/aaron/sparx/SparxMSSQLConverter.java
@@ -6,6 +6,7 @@ import aaron.sparx.config.Config;
 import aaron.sparx.config.MSSQLDB;
 import aaron.sparx.model.*;
 import aaron.util.Util;
+import com.microsoft.sqlserver.jdbc.SQLServerException;
 import org.apache.commons.lang3.StringUtils;
 
 import java.io.IOException;
@@ -57,44 +58,57 @@ public class SparxMSSQLConverter extends AbstractSparxConverter {
     public Model convert() throws IOException {
         LocalDateTime now = LocalDateTime.now();
         //jdbc:sqlserver://[serverName[\instanceName][:portNumber]][;property=value[;property=value]]
-        String url = "jdbc:sqlserver://" + host + (StringUtils.isNotBlank(instance) ? "\\" +instance : "") + ":" + port + ";encrypt=true" + ";trustServerCertificate=" + trustServerCertificate + ";databaseName=" + databaseName;
+        String url = "jdbc:sqlserver://" + host + (StringUtils.isNotBlank(instance) ? "\\" +instance : "") + ":" + port + ";databaseName=" + databaseName;
         switch (authenticationType) {
             case AD_LOGIN ->   url = url + ";authenticationScheme=NTLM;integratedSecurity=true";
             case SQL_LOGIN ->  url = url + ";authenticationScheme=NativeAuthentication;integratedSecurity=false";
         }
-        try (Connection connection = DriverManager.getConnection(url, username, password)) {
-            String sha1 = Util.createSHA1(url, now.toInstant(ZoneOffset.UTC));
-            context.setFileHash(sha1);
+        String secureUrl = url + ";encrypt=true" + ";trustServerCertificate=" + trustServerCertificate;
+        String[] urls = new String[]{secureUrl, url};
+        for (int i = 0; i < urls.length; i++) {
+            if (i == 0) {
+                logger.info("try to establish secure connection to {}", host);
+            } else {
+                logger.warn("Could not establish secure connection to {}. Try insecure connection", host);
+            }
+            String jdbcUrl = urls[i];
+            try (Connection connection = DriverManager.getConnection(jdbcUrl, username, password)) {
+                String sha1 = Util.createSHA1(jdbcUrl, now.toInstant(ZoneOffset.UTC));
+                context.setFileHash(sha1);
 
-            handleTable(sha1, now, connection, this::processSystem, "SELECT * FROM " + EASystem.TABLE_NAME);
+                handleTable(sha1, now, connection, this::processSystem, "SELECT * FROM " + EASystem.TABLE_NAME);
 
-            handleTable(sha1, now, connection, this::processPackages, "SELECT * FROM " + EAPackage.TABLE_NAME);
+                handleTable(sha1, now, connection, this::processPackages, "SELECT * FROM " + EAPackage.TABLE_NAME);
 
-            handleTable(sha1, now, connection, this::processDiagrams, "SELECT * FROM " + EADiagram.TABLE_NAME);
+                handleTable(sha1, now, connection, this::processDiagrams, "SELECT * FROM " + EADiagram.TABLE_NAME);
 
-            handleTable(sha1, now, connection, this::processObjects, "SELECT * FROM " + EAObject.TABLE_NAME);
-            handleTable(sha1, now, connection, this::processObjectProperties, "SELECT * FROM " + EAObjectProperty.TABLE_NAME);
-            handleTable(sha1, now, connection, this::processObjectConstraints, "SELECT * FROM " + EAObjectConstraint.TABLE_NAME);
+                handleTable(sha1, now, connection, this::processObjects, "SELECT * FROM " + EAObject.TABLE_NAME);
+                handleTable(sha1, now, connection, this::processObjectProperties, "SELECT * FROM " + EAObjectProperty.TABLE_NAME);
+                handleTable(sha1, now, connection, this::processObjectConstraints, "SELECT * FROM " + EAObjectConstraint.TABLE_NAME);
 
-            handleTable(sha1, now, connection, this::processConnectors, "SELECT * FROM " + EAConnector.TABLE_NAME);
-            handleTable(sha1, now, connection, this::processConnectorTags, "SELECT * FROM " + EAConnectorTag.TABLE_NAME);
-            handleTable(sha1, now, connection, this::processConnectorConstraints, "SELECT * FROM " + EAConnectorConstraint.TABLE_NAME);
+                handleTable(sha1, now, connection, this::processConnectors, "SELECT * FROM " + EAConnector.TABLE_NAME);
+                handleTable(sha1, now, connection, this::processConnectorTags, "SELECT * FROM " + EAConnectorTag.TABLE_NAME);
+                handleTable(sha1, now, connection, this::processConnectorConstraints, "SELECT * FROM " + EAConnectorConstraint.TABLE_NAME);
 
-            handleTable(sha1, now, connection, this::processDiagramObjects, "SELECT * FROM " + EADiagramObject.TABLE_NAME);
-            handleTable(sha1, now, connection, this::processDiagramLinks, "SELECT * FROM " + EADiagramLink.TABLE_NAME);
+                handleTable(sha1, now, connection, this::processDiagramObjects, "SELECT * FROM " + EADiagramObject.TABLE_NAME);
+                handleTable(sha1, now, connection, this::processDiagramLinks, "SELECT * FROM " + EADiagramLink.TABLE_NAME);
 
-            handleTable(sha1, now, connection, this::processOperations, "SELECT * FROM " + EAOperation.TABLE_NAME);
-            handleTable(sha1, now, connection, this::processOperationTags, "SELECT * FROM " + EAOperationTag.TABLE_NAME);
-            handleTable(sha1, now, connection, this::processOperationParams, "SELECT * FROM " + EAOperationParams.TABLE_NAME);
+                handleTable(sha1, now, connection, this::processOperations, "SELECT * FROM " + EAOperation.TABLE_NAME);
+                handleTable(sha1, now, connection, this::processOperationTags, "SELECT * FROM " + EAOperationTag.TABLE_NAME);
+                handleTable(sha1, now, connection, this::processOperationParams, "SELECT * FROM " + EAOperationParams.TABLE_NAME);
 
-            handleTable(sha1, now, connection, this::processAttributes, "SELECT * FROM " + EAAttribute.TABLE_NAME);
-            handleTable(sha1, now, connection, this::processAttributeTags, "SELECT * FROM " + EAAttributeTag.TABLE_NAME);
-            handleTable(sha1, now, connection, this::processAttributeConstraints, "SELECT * FROM " + EAAttributeConstraint.TABLE_NAME);
+                handleTable(sha1, now, connection, this::processAttributes, "SELECT * FROM " + EAAttribute.TABLE_NAME);
+                handleTable(sha1, now, connection, this::processAttributeTags, "SELECT * FROM " + EAAttributeTag.TABLE_NAME);
+                handleTable(sha1, now, connection, this::processAttributeConstraints, "SELECT * FROM " + EAAttributeConstraint.TABLE_NAME);
 
-            handleTable(sha1, now, connection, this::processXRefs, "SELECT * FROM " + EAXref.TABLE_NAME);
-            return model;
-        } catch (SQLException | NoSuchAlgorithmException e) {
-            throw new RuntimeException(e);
+                handleTable(sha1, now, connection, this::processXRefs, "SELECT * FROM " + EAXref.TABLE_NAME);
+                return model;
+            } catch (SQLServerException serverException) {
+                continue;
+            } catch (SQLException | NoSuchAlgorithmException e) {
+                throw new RuntimeException(e);
+            }
         }
+        return null;
     }
 }
